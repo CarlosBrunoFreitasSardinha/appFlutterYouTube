a88ff14c1b2c1e5babd06cf8e19fde0eab87bf21
carlosifto <carlosifto@gmail.com>
2020-04-16 22:14:28 -0300

    executando videos

--- lib/views/Inicial.dart
+++ lib/views/Inicial.dart
@@ -2,9 +2,9 @@ import 'package:appyoutube/api.dart';
 import 'package:appyoutube/model/Video.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_youtube/flutter_youtube.dart';
 
 class Inicial extends StatefulWidget {
-
   String pesquisa;
 
   Inicial(this.pesquisa);
@@ -14,9 +14,7 @@ class Inicial extends StatefulWidget {
 }
 
 class _inicialState extends State<Inicial> {
-
   _listarVideos(String pesquisa) {
-
     Api api = Api();
     return api.pesquisar(pesquisa);
   }
@@ -29,7 +27,6 @@ class _inicialState extends State<Inicial> {
         switch (snapshot.connectionState) {
           case ConnectionState.none:
           case ConnectionState.waiting:
-
             return Center(
               child: CircularProgressIndicator(),
             );
@@ -37,37 +34,43 @@ class _inicialState extends State<Inicial> {
 
           case ConnectionState.active:
           case ConnectionState.done:
-
             if (snapshot.hasData) {
-
               return ListView.separated(
-                  itemBuilder: (context, index){
-                    List<Video> videos = snapshot.data;
-                    Video video = videos[index];
-                        return Column(
-                          children: <Widget>[
-                            Container(
-                              height: 200,
-                              decoration: BoxDecoration(
-                                image: DecorationImage(
-                                  image: NetworkImage(video.imagem),
-                                  fit: BoxFit.cover,
-                                ),
-                              ),
+                itemBuilder: (context, index) {
+                  List<Video> videos = snapshot.data;
+                  Video video = videos[index];
+                  return GestureDetector(
+                    onTap: () {
+                      FlutterYoutube.playYoutubeVideoById(
+                          apiKey: CHAVE_YOUTUBE_API,
+                          videoId: video.id,
+                          autoPlay: true,
+                          fullScreen: true);
+                    },
+                    child: Column(
+                      children: <Widget>[
+                        Container(
+                          height: 200,
+                          decoration: BoxDecoration(
+                            image: DecorationImage(
+                              image: NetworkImage(video.imagem),
+                              fit: BoxFit.cover,
                             ),
-                            ListTile(
-                              title: Text(video.titulo),
-                              subtitle: Text(video.descricao),
-                            ),
-                          ],
-                        );
-                      },
-                  separatorBuilder:
-                      (context, index)=> Divider(
-                        height: 3,
-                        color: Colors.red,
-                      ),
-                  itemCount: snapshot.data.length,
+                          ),
+                        ),
+                        ListTile(
+                          title: Text(video.titulo),
+                          subtitle: Text(video.descricao),
+                        ),
+                      ],
+                    ),
+                  );
+                },
+                separatorBuilder: (context, index) => Divider(
+                  height: 3,
+                  color: Colors.red,
+                ),
+                itemCount: snapshot.data.length,
               );
             } else
               return Center(
